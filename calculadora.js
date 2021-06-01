66d4e1f304c889b0725abedd64fbff159a4c7668
FacuGarrote <ceciefacugmail.com>
2021-05-31 21:39:11 -0300

    Este es mi primer commit

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+let resultadoDeSuma = require('./operadores/sumar')
+let suma = resultadoDeSuma(3,6)
+console.log(suma);
+
+
+let resultadoDeRestar = require ('./operadores/resta')
+let restar = resultadoDeRestar(5,6)  
+console.log(restar);
+
+let resultadoDeMultiplicar = require ('./operadores/multiplicar')
+let multi = resultadoDeMultiplicar(2,5)
+console.log(multi);
+
+let resultadoDeDividir = require ('./operadores/dividir')
+let divid = resultadoDeDividir(6,0)
+console.log(divid);
